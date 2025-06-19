368dcab8ca3d3b911cc1a332ff8b6c610a2df2ca
MPhoenixs <mayankluffy1234@gmail.com>
2025-06-19 12:46:16 +0530

    Add similarity.pkl to .gitignore

--- setup.sh
+++
@@ -1,1 +0,0 @@
-```bash mkdir -p ~/.streamlit/ echo "[general]\nemail = "mayankluffy1234@gmail.com"\n" > ~/.streamlit/credentials.toml echo "[server]\nheadless = true\nenableCORS=false\nport = $PORT\n" > ~/.streamlit/config.toml
